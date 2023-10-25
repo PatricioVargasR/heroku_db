dc0b527348095e0f499eab989ddcbbe0b6e4beaf
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2023-10-25 10:31:45 -0600

    Update to MySQL

--- main.py
+++ main.py
@@ -1,73 +1,80 @@
 import fastapi
-import sqlite3
+import mysql.connector
 from pydantic import BaseModel
-
-# Crea la base de datos
-conn = sqlite3.connect("contactos.db")
 
 app = fastapi.FastAPI()
 
 class Contacto(BaseModel):
-    email : str
-    nombre : str
-    telefono : str
+    email: str
+    nombre: str
+    telefono: str
 
+# Crear la conexión a la base de datos
+conn = mysql.connector.connect(
+    user='root',
+    password='',
+    host='localhost',
+    database='contactos'
+)
 
 @app.get("/")
 def inicio():
-    return {'Developer by':'Patricio Vargas f:'}
+    return {'Developer by': 'Patricio Vargas f:'}
 
 # Rutas para las operaciones CRUD
 
 @app.post("/contactos")
 async def crear_contacto(contacto: Contacto):
     """Crea un nuevo contacto."""
-    # TODO Inserta el contacto en la base de datos y responde con un mensaje
+    # Insertar el contacto en la base de datos y responder con un mensaje
     c = conn.cursor()
-    c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
-              (contacto.email, contacto.nombre, contacto.telefono))
+    add_data = (
+        'INSERT INTO contactos (email, nombre, telefono) VALUES (%s, %s, %s)',
+        (contacto.email, contacto.nombre, contacto.telefono)
+    )
+    c.execute(*add_data)
     conn.commit()
     return contacto
 
 @app.get("/contactos")
 async def obtener_contactos():
     """Obtiene todos los contactos."""
-    # TODO Consulta todos los contactos de la base de datos y los envia en un JSON
+    # Consultar todos los contactos de la base de datos y enviarlos en un JSON
     c = conn.cursor()
     c.execute('SELECT * FROM contactos;')
     response = []
     for row in c:
-        contacto = {"email":row[0],"nombre":row[1], "telefono":row[2]}
+        contacto = {"email": row[0], "nombre": row[1], "telefono": row[2]}
         response.append(contacto)
     return response
-
 
 @app.get("/contactos/{email}")
 async def obtener_contacto(email: str):
     """Obtiene un contacto por su email."""
-    # Consulta el contacto por su email
+    # Consultar el contacto por su email
     c = conn.cursor()
-    c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+    c.execute('SELECT * FROM contactos WHERE email = %s', (email,))
     contacto = None
     for row in c:
-        contacto = {"email":row[0],"nombre":row[1],"telefono":row[2]}
+        contacto = {"email": row[0], "nombre": row[1], "telefono": row[2]}
     return contacto
-
 
 @app.put("/contactos/{email}")
 async def actualizar_contacto(email: str, contacto: Contacto):
     """Actualiza un contacto."""
     c = conn.cursor()
-    c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
-              (contacto.nombre, contacto.telefono, email))
+    c.execute(
+        'UPDATE contactos SET nombre = %s, telefono = %s WHERE email = %s',
+        (contacto.nombre, contacto.telefono, email)
+    )
     conn.commit()
     return contacto
 
 @app.delete("/contactos/{email}")
 async def eliminar_contacto(email: str):
     """Elimina un contacto."""
-    # TODO Elimina el contacto de la base de datos
+    # Eliminar el contacto de la base de datos
     c = conn.cursor()
-    c.execute('DELETE FROM contactos WHERE email = ?', (email,))
+    c.execute('DELETE FROM contactos WHERE email = %s', (email,))
     conn.commit()
-    return {"mensaje":"Contacto eliminado"}
+    return {"mensaje": "Contacto eliminado"}
